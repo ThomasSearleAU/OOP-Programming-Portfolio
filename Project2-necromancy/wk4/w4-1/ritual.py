e878bba039284c6d6464ed22e6dc8076b8e2eaf4
ThomasSearleAU <a3216707@adelaide.edu.au>
2026-09-10 15:00:48 +0930

    store defaults

--- Project2-necromancy/wk4/w4-1/ritual.py
+++ Project2-necromancy/wk4/w4-1/ritual.py
@@ -1,5 +1,5 @@
 class Ritual:
-    def __init__(self, undead_name, ritual_name, init_health, init_power, necrotic_rune, spirit_rune, bone_rune, flesh_rune, ectoplasm):
+    def __init__(self, undead_name, ritual_name, init_health, init_power, necrotic_rune=0, spirit_rune=0, bone_rune=0, flesh_rune=0, ectoplasm=1):
         self.undead_name = undead_name
         self.ritual_name = ritual_name
         self.init_health = init_health
