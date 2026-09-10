5fe1eba81adb86ecfd94ae3edb137b702a2f3652
ThomasSearleAU <a3216707@adelaide.edu.au>
2026-09-10 15:00:13 +0930

    defined values for Ritual class

--- Project2-necromancy/wk4/w4-1/ritual.py
+++ Project2-necromancy/wk4/w4-1/ritual.py
@@ -1,10 +1,14 @@
 class Ritual:
-
-    def __init__(self):
+    def __init__(self, undead_name, ritual_name, init_health, init_power, necrotic_rune, spirit_rune, bone_rune, flesh_rune, ectoplasm):
+        self.undead_name = undead_name
+        self.ritual_name = ritual_name
+        self.init_health = init_health
+        self.init_power = init_power
         self.costs = {
-            'Necrotic Rune': 1,
-            'Spirit Rune': 2,
-            'Bone Rune': 3,
-            'Flesh Rune': 4,
-            'Ectoplasm': 5
+            'Necrotic Rune': necrotic_rune,
+            'Spirit Rune': spirit_rune,
+            'Bone Rune': bone_rune,
+            'Flesh Rune': flesh_rune,
+            'Ectoplasm': ectoplasm
         }
+    
