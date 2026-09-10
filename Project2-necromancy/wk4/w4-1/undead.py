250f148b97194366d7aebf7bee3480cff980da46
ThomasSearleAU <a3216707@adelaide.edu.au>
2026-09-10 14:13:57 +0930

    init of undead class consts and leveling consts

--- Project2-necromancy/wk4/w4-1/undead.py
+++ Project2-necromancy/wk4/w4-1/undead.py
@@ -1,6 +1,18 @@
 class Undead:
-    def __init__(self, name, hp, power):
-        self.name
-        self.hp
-        self.power
-        self.level = 1
+    MAX_HEALTH = 10
+    MIN_HEALTH = 0
+
+    MAX_POWER = 10
+    MIN_POWER = 0
+
+    MAX_LEVEL = 999
+
+    HEALTH_PER_LEVEL = 2
+    POWER_PER_LEVEL = 5
+
+    def __init__(self, unit_id, name, hp, power):
+        self.__name
+        self.__health
+        self.__power
+        self.__level = 1
+
